80a133b09d5295915b1a28cff007886de875a556
octopod <octopodsquad@gmail.com>
2024-06-30 01:06:05 -1000

    Add forced integer scale parameter to simple scene

--- project/scenes/simple_scene.gd
+++ project/scenes/simple_scene.gd
@@ -1,17 +1,16 @@
 extends M8Scene
 
-var m8_client: M8GD
+@export_range(0, 6) var force_integer_scale: int:
+    set(value):
+        force_integer_scale = value
 
-func initialize(main_: M8SceneDisplay):
-    super(main_)
+func initialize(p_main: M8SceneDisplay):
+    super(p_main)
 
     # %TextureRect.texture = _display.m8_display_texture
     %TextureRect.texture = main.m8_client.get_display_texture()
-    m8_client = main.m8_client
 
-func _process(_delta):
-
-    RenderingServer.set_default_clear_color(m8_client.get_background_color())
+func get_auto_integer_scale() -> int:
 
     var window_size = DisplayServer.window_get_size()
     var texture = %TextureRect.texture
@@ -20,4 +19,13 @@ func _process(_delta):
     while ((intscale + 1) * texture.get_size().x <= window_size.x and (intscale + 1) * texture.get_size().y <= window_size.y):
         intscale += 1
 
-    %TextureRect.custom_minimum_size = intscale * texture.get_size();
+    return intscale
+
+func _process(_delta):
+
+    RenderingServer.set_default_clear_color(main.m8_client.get_background_color())
+
+    if force_integer_scale == 0:
+        %TextureRect.custom_minimum_size = %TextureRect.texture.get_size() * get_auto_integer_scale();
+    else:
+        %TextureRect.custom_minimum_size = %TextureRect.texture.get_size() * force_integer_scale;
