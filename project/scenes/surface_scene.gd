a71298d3835d3c73ac4486a0cb4726ba4751bec9
Branden Akana <branden.akana@gmail.com>
2024-11-30 18:39:29 -1000

    Surface scene: Add more texture options and custom texture support

--- project/scenes/surface_scene.gd
+++ project/scenes/surface_scene.gd
@@ -1,5 +1,7 @@
 extends M8Scene
 
+const GRID_OVERLAY_MATERIAL := preload("res://assets/grid_overlay.tres")
+
 @onready var camera: HumanizedCamera3D = %Camera3D
 
 @export var model_screen_emission := 0.25:
@@ -8,10 +10,28 @@ extends M8Scene
 			get_device_model().set_screen_emission(value)
 		model_screen_emission = value
 
-@export var surface_color := Color(0.0, 0.0, 0.0):
+@export var surface_color := Color(1.0, 1.0, 1.0):
 	set(value):
 		surface_color = value
-		%SurfaceMesh.material_override.albedo_color = value
+		if %SurfaceMesh.material_override:
+			%SurfaceMesh.material_override.albedo_color = value
+
+var surface_material_custom: StandardMaterial3D = null
+
+@export var surface_enable_grid := false:
+	set(value):
+		surface_enable_grid = value
+		if value:
+			%SurfaceMesh.material_overlay = GRID_OVERLAY_MATERIAL
+			%SurfaceMesh.material_overlay.albedo_color = surface_grid_color
+		else:
+			%SurfaceMesh.material_overlay = null
+
+@export var surface_grid_color := Color.WHITE:
+	set(value):
+		surface_grid_color = value
+		if %SurfaceMesh.material_overlay:
+			%SurfaceMesh.material_overlay.albedo_color = value
 
 @export var enable_grass := false:
 	set(value):
@@ -74,10 +94,72 @@ func init(p_main: M8SceneDisplay) -> void:
 	camera.init(main)
 
 func init_menu(menu: SceneMenu) -> void:
-	menu.add_exports_from(self)
+
+	menu.add_auto("model_screen_emission")
+
+	menu.add_section("Surface")
+	menu.add_option_custom("surface_mode", 0, [
+		"Wood",
+		"Stone",
+		"M8 Display",
+		"Custom"
+	], set_surface_mode)
+	menu.add_file_custom("surface_texture", "", func(path: String) -> void:
+		var texture := load_media_to_texture_rect(path, %VideoStreamPlayer)
+		if texture is Texture2D:
+			var material := StandardMaterial3D.new()
+			material.albedo_texture = texture
+			surface_material_custom = material
+			load_custom_texture()
+	)
+	menu.add_auto("surface_color")
+	menu.add_auto("surface_enable_grid")
+	menu.add_auto("surface_grid_color")
+
+	menu.add_section("Decorations")
+	menu.add_auto("enable_plant")
+	menu.add_auto("enable_grass")
+
+	menu.add_section("Lighting")
+	menu.add_auto("enable_directional_light")
+	menu.add_auto("directional_light_color")
+	menu.add_auto("enable_lamp_light")
+	menu.add_auto("lamp_light_color")
+	menu.add_auto("enable_left_light")
+	menu.add_auto("left_light_color")
+	menu.add_auto("enable_right_light")
+	menu.add_auto("right_light_color")
+
 
 func _physics_process(delta: float) -> void:
 
 	if main.is_menu_open(): return
 
 	camera.update(delta)
+
+func set_surface_mode(index: int) -> void:
+	match index:
+		0: # wood
+			%SurfaceMesh.material_override = load("res://assets/ambientcg/wood051.tres")
+			%SurfaceMesh.material_override.albedo_color = surface_color
+		1: # stone
+			%SurfaceMesh.material_override = load("res://assets/ambientcg/asphalt010.tres")
+			%SurfaceMesh.material_override.albedo_color = surface_color
+		2: # display
+			%SurfaceMesh.material_override = StandardMaterial3D.new()
+			%SurfaceMesh.material_override.albedo_texture = main.m8_client.get_display_texture()
+			%SurfaceMesh.material_override.albedo_color = surface_color
+			%SurfaceMesh.material_override.uv1_triplanar = true
+			%SurfaceMesh.material_override.uv1_scale = Vector3(0.125, 0.125, 0.125)
+		3: # custom
+			load_custom_texture()
+
+func load_custom_texture() -> void:
+	if surface_material_custom:
+		%SurfaceMesh.material_override = surface_material_custom
+		%SurfaceMesh.material_override.albedo_color = surface_color
+		%SurfaceMesh.material_override.uv1_triplanar = true
+		%SurfaceMesh.material_override.uv1_scale = Vector3(0.125, 0.125, 0.125)
+	else:
+		%SurfaceMesh.material_override = StandardMaterial3D.new()
+		%SurfaceMesh.material_override.albedo_color = surface_color
